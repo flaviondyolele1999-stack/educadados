7a487ff2a3d0014b910057dcfe91129b37b4995f
Flávio Ndyolele <flaviondyolele1999@gmail.com>
2026-09-14 00:36:11 +0100

    forcar https atras do proxy do render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,9 @@ return Application::configure(basePath: dirname(__DIR__))
             AddLinkHeadersForPreloadedAssets::class,
         ]);
     })
+    ->withMiddleware(function (Middleware $middleware) {
+         $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
